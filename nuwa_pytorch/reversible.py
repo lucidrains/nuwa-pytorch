f86ae2edaf65dbc3af1fff4a4f7b9b22f278eea6
Phil Wang <lucidrains@gmail.com>
2022-01-09 14:32:36 -0800

    cleanup

--- nuwa_pytorch/reversible.py
+++ nuwa_pytorch/reversible.py
@@ -123,23 +123,6 @@ class _ReversibleFunction(Function):
             y, dy = block.backward_pass(y, dy, **kwargs)
         return dy, None, None
 
-
-class SequentialSequence(nn.Module):
-    def __init__(self, layers, args_route = {}):
-        super().__init__()
-        assert all(len(route) == len(layers) for route in args_route.values()), 'each argument route map must have the same depth as the number of sequential layers'
-        self.layers = layers
-        self.args_route = args_route
-
-    def forward(self, x, **kwargs):
-        args = route_args(self.args_route, kwargs, len(self.layers))
-        layers_and_args = list(zip(self.layers, args))
-
-        for (f, g), (f_args, g_args) in layers_and_args:
-            x = x + f(x, **f_args)
-            x = x + g(x, **g_args)
-        return x
-
 class ReversibleSequence(nn.Module):
     def __init__(self, blocks, args_route = {}):
         super().__init__()
